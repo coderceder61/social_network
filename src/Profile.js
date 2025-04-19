e336ab9d97c7ac13ed5dfdcbcc0c49874855bfe2
coderceder61 <codercoder61@gmail.com>
2025-04-19 05:13:04 +0100

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -1243,19 +1243,7 @@ const getUserDataak = async (username) => {
 
       const handleClickOutside = (event) => {
         let commentsBox = document.querySelectorAll('.allp') 
-         if(event.target===popup.current){
-               console.log(1)
-              if(upsd.current && upsd.current.style.display==='none'){
-upsd.current.style.display='block'
-//setVisible(true)
-               console.log(2)
-                 
-}else{
-//setVisible(false)
-               console.log(3)
-
-upsd.current.style.display='none'
-}
+         
         commentsBox.forEach((element) => {
           // console.log(element,)
           if(element.style.display == 'flex' && event.target.parentElement.parentElement.parentElement!==element && event.target.parentElement.parentElement.parentElement.parentElement!==element && event.target.tagName !== 'SPAN'){
