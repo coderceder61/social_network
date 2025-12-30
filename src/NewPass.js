a43722614f3117b6ad865b53e8884442ff1a92fd
coderceder61 <codercoder61@gmail.com>
2025-12-30 18:48:55 +0100

    Update NewPass.js

--- src/NewPass.js
+++ src/NewPass.js
@@ -51,7 +51,7 @@ function Home() {
             {good1 && <span style={{margin:'10px 20px',display:'inline-block',color:'rgb(177, 7, 72)',border:'1px solid rgb(203, 184, 190)',padding:'15px 10px',backgroundColor:'#f8d7da',width:'90%'}}>enter your new password</span>}
             <div style={{margin:'10px 20px',display:'flex',alignItems:'center',justifyContent:'space-between'}}><input autocomplete="off" style={{padding:'5px 10px',backgroundColor:'#0b5ed7',color:'white',borderRadius:'5px'}} type="submit" value="Change Password" name="sign"/></div>
 {good3 && <div className="loader2"></div>}
-  {message && <p style={{    background:data.status==="error"? '#ff9090':'#5ded5d',
+  {message && <p style={{    background:message.status==="error"? '#ff9090':'#5ded5d',
     margin: '10px 20px',
     color: 'white',
     borderRadius: '5px',
@@ -59,7 +59,7 @@ function Home() {
     width: 'fit-content',
     display: 'flex',
     justifyContent: 'center',
-    textAlign: 'center'}}>{data.status!=="error"?"Password Updated":"Invalid or expired token"}</p>}
+    textAlign: 'center'}}>{message.status!=="error"?"Password Updated":"Invalid or expired token"}</p>}
             <Link to='/' style={{margin:'10px 20px'}} href=""><i style={{marginRight:'5px'}} className="fa-solid fa-circle-arrow-left"></i>Go Back To Login</Link>
         </form>
     </div>
