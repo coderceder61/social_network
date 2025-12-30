40a82ffe868358ce61e92699a0476eb3b418b5f9
coderceder61 <codercoder61@gmail.com>
2025-12-30 21:40:07 +0100

    Update Forgot.js

--- src/Forgot.js
+++ src/Forgot.js
@@ -4,17 +4,25 @@ import {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
+import ReCAPTCHA from "react-google-recaptcha";
 function Home() {
+    const [captchaToken, setCaptchaToken] = useState(null);
     const [good1,setGood1] = useState(false)
     const [good3,setGood3] = useState(false)
     const [good4,setGood4] = useState(false)
     const [email,setEmail] = useState('')
     const navigate = useNavigate();
-   
+   const handleCaptchaChange = (token) => {
+        setCaptchaToken(token)
+      setFormData(prev => ({ ...prev, captchaToken: token }));
+    }
     const handleSubmit = async (e) => {
         setGood3(false)
       e.preventDefault();
+        if (!captchaToken) {
+      alert("Please verify that you are not a robot");
+      return;
+    }
         if(email===''){
           setGood1(true)
             setGood3(false)
@@ -44,7 +52,12 @@ function Home() {
 {good4 && <div className="loader2"></div>}
 
             {good1 && <span style={{margin:'10px 20px',display:'inline-block',color:'rgb(177, 7, 72)',border:'1px solid rgb(203, 184, 190)',padding:'15px 10px',backgroundColor:'#f8d7da',width:'90%'}}>enter your email id !</span>}
-            
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                  <ReCAPTCHA
+                    sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+                    onChange={handleCaptchaChange}
+                  />
+                </div>
             <div style={{margin:'10px 20px',display:'flex',alignItems:'center',justifyContent:'space-between'}}><input autocomplete="off" style={{padding:'5px 10px',backgroundColor:'#0b5ed7',color:'white',borderRadius:'5px'}} type="submit" value="Send Request" name="sign"/></div>
             <Link to='/' style={{margin:'10px 20px'}} href=""><i style={{marginRight:'5px'}} className="fa-solid fa-circle-arrow-left"></i>Go Back To Login</Link>
         </form>
